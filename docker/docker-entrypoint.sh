a3b89f6fbceaf528d368728c28b3c82da61cfac9
Ymage <heltem@o2php.com>
2026-05-24 22:30:47 +0200

    fix(entrypoint): use HOST/PORT env vars at startup time

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,10 +4,9 @@ set -Eeuo pipefail
 err_report() {
   echo "errexit ($1) on line $2" >&2
 }
-
 trap 'err_report $? ${LINENO}' ERR
 
 cd /var/www
 tar xfz /application.tar.gz
 
-npx next start
+npx next start -H "${HOST:-0.0.0.0}" -p "${PORT:-3000}"
