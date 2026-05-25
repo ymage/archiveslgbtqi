92f22a71b7946787fb0fd05c420be41e82a0e53b
Ymage <heltem@o2php.com>
2026-05-25 14:20:45 +0200

    build(docker): simplify runtime by copying files directly instead of tar archive

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -8,9 +8,4 @@ trap 'err_report $? ${LINENO}' ERR
 
 cd /var/www
 
-# Clean up and extract application
-echo "Begin : Unarchive archiveslgbtqi webapp"
-tar xvf /application.tar.gz
-echo "End : Unarchive archiveslgbtqi webapp"
-
 npx next start -H "${HOST:-0.0.0.0}" -p "${PORT:-3000}"
